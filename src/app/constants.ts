658cfb0851bbf0e6bc28eb161a5ca0e8ad62c8f0
juan-hussain <jooan84@gmail.com>
2026-06-22 19:13:22 +0000

    Use same-origin frontend API base URL

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,7 +1,7 @@
 export class Constants {
   // Server URL - using HTTPS since the application is served over HTTPS
   // In production, this should be set via environment files
-  public static SERVER_URL = 'https://116.202.96.11';
+  public static SERVER_URL = '';
   public static REQUEST_TIMEOUT = 30000;
   public static DISABLE_NO_INTERNET_ALERT = false;
 }
